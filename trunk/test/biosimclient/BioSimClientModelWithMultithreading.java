8c5af1d6ba214383c1845b3568a3af414f47cc2a
donenrique <donenrique@8ab3efae-b829-4e4f-8a0c-45b54eb44eb3>
2020-06-08 14:09:19 +0000

    Enabling multi-threading (not operational yet)

--- trunk/test/biosimclient/BioSimClientModelWithMultithreading.java
+++ trunk/test/biosimclient/BioSimClientModelWithMultithreading.java
@@ -28,9 +28,12 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
+import biosimclient.BioSimClientTest.FakeLocation;
 
-public class BioSimClientModelWithParametersTest {
 
+public class BioSimClientModelWithMultithreading {
+
+	
 	/*
 	 * Tests if the weather generation over past and future time intervals.
 	 */
@@ -38,32 +41,45 @@ public class BioSimClientModelWithParametersTest {
 	public void testingWithDegreeDaysAbove5C() throws BioSimClientException, BioSimServerException {
 		BioSimClient.setMultithreadingEnabled(false);
 		List<BioSimPlot> locations = new ArrayList<BioSimPlot>();
-		locations.add(BioSimClientTestsOnNormals.getPlots().get(0));
+		for (int i = 0; i < 100; i++) {
+			FakeLocation loc = new FakeLocation(45 + i * .05,
+					-74 + i * .05,
+					300 + 1 * i);
+			locations.add(loc);
+		}
+ 
 		int initialDateYr = 2000;
 		BioSimParameterMap parms = new BioSimParameterMap();
 		parms.addParameter("LowerThreshold", 5);
+		long initial = System.currentTimeMillis();
 		LinkedHashMap<BioSimPlot, BioSimDataSet> teleIORefs = BioSimClient.getModelOutput(initialDateYr, 2001, locations, null, null, "DegreeDay_Annual", true, parms);
+		double elapsedTime = (System.currentTimeMillis() - initial) * .001;
+		System.out.println("Elapsed time single threading = " + elapsedTime);
+		
+		initial = System.currentTimeMillis();
+		BioSimClient.setMultithreadingEnabled(true);
+		LinkedHashMap<BioSimPlot, BioSimDataSet> teleIORefs2 = BioSimClient.getModelOutput(initialDateYr, 2001, locations, null, null, "DegreeDay_Annual", true, parms);
+		elapsedTime = (System.currentTimeMillis() - initial) * .001;
+		System.out.println("Elapsed time multi threading = " + elapsedTime);
 		
 		for (BioSimPlot plot : teleIORefs.keySet()) {
 			BioSimDataSet firstDataSet = teleIORefs.get(plot);
+			BioSimDataSet secondDataSet = teleIORefs2.get(plot);
+			
 			Assert.assertTrue("Is there at least one observation", firstDataSet.getNumberOfObservations() > 0);
-			System.out.println("There is at least one observation");
+//			System.out.println("There is at least one observation");
 
-			int dateFieldIndex = firstDataSet.getFieldNames().indexOf("Year");
+			Assert.assertTrue("Same number of observations", firstDataSet.getNumberOfObservations() == secondDataSet.getNumberOfObservations());
+//			System.out.println("There is at least one observation");
+
 			int ddFieldIndex = firstDataSet.getFieldNames().indexOf("DD");
 			for (int i = 0; i < firstDataSet.getNumberOfObservations(); i++) {
-				int dateYr = (Integer) firstDataSet.getValueAt(i, dateFieldIndex);
-				double actualDD = (Double) firstDataSet.getValueAt(i, ddFieldIndex);
-				double expectedDD;
-				if (dateYr == 2000) {
-					expectedDD = 1575d;
-				} else {
-					expectedDD = 1847.15;
-				}
+				double actualDD = (Double) secondDataSet.getValueAt(i, ddFieldIndex);
+				double expectedDD = (Double) firstDataSet.getValueAt(i, ddFieldIndex);
 				Assert.assertEquals("Testing degree-days above 5C",	expectedDD,	actualDD, 1E-8);
 			}
-			System.out.println("Degree-days above 5C successfully tested!");
 		}
+		System.out.println("Degree-days above 5C successfully tested with multithreading!");
 		
 	}
 
