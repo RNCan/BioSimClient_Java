e1fd5f6e5b20c576b5a5c9a9956a1339b1fd7204
donenrique <donenrique@8ab3efae-b829-4e4f-8a0c-45b54eb44eb3>
2020-08-13 13:58:33 +0000

    Removing the test for multithreading. It is now handled on the server side.

--- trunk/test/biosimclient/BioSimClientModelWithMultithreading.java
+++ trunk/test/biosimclient/BioSimClientModelWithMultithreading.java
@@ -22,23 +22,18 @@
 package biosimclient;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-
-import org.junit.Assert;
-import org.junit.Test;
 
 import biosimclient.BioSimClientTest.FakeLocation;
 
 
 public class BioSimClientModelWithMultithreading {
 
-	
 	/*
-	 * Tests if the weather generation over past and future time intervals.
+	 * A reference test for multithreading on the server side
+	 * With 4 processes on the server side this takes 27.5 sec.
 	 */
-	@Test
-	public void testingWithDegreeDaysAbove5C() throws BioSimClientException, BioSimServerException {
+	public static void testingWithDegreeDaysAbove5C() throws BioSimClientException, BioSimServerException {
 		BioSimClient.setMultithreadingEnabled(false);
 		List<BioSimPlot> locations = new ArrayList<BioSimPlot>();
 		for (int i = 0; i < 100; i++) {
@@ -52,39 +47,15 @@ public class BioSimClientModelWithMultithreading {
 		BioSimParameterMap parms = new BioSimParameterMap();
 		parms.addParameter("LowerThreshold", 5);
 		long initial = System.currentTimeMillis();
-		LinkedHashMap<BioSimPlot, BioSimDataSet>  teleIORefs = BioSimClient.getModelOutput(initialDateYr, 2001, locations, null, null, "DegreeDay_Annual", true, parms);
+		BioSimClient.getModelOutput(initialDateYr, 2010, locations, null, null, "DegreeDay_Annual", true, parms);
 		double elapsedTime = (System.currentTimeMillis() - initial) * .001;
-		System.out.println("Elapsed time single threading = " + elapsedTime);
-		
-		initial = System.currentTimeMillis();
-		BioSimClient.setMultithreadingEnabled(true);
-		LinkedHashMap<BioSimPlot, BioSimDataSet> teleIORefs2 = BioSimClient.getModelOutput(initialDateYr, 2001, locations, null, null, "DegreeDay_Annual", true, parms);
-		elapsedTime = (System.currentTimeMillis() - initial) * .001;
-		System.out.println("Elapsed time multi threading = " + elapsedTime);
-		
-		for (BioSimPlot plot : teleIORefs.keySet()) {
-			BioSimDataSet firstDataSet = teleIORefs.get(plot);
-			BioSimDataSet secondDataSet = teleIORefs2.get(plot);
-			
-			Assert.assertTrue("Is there at least one observation", firstDataSet.getNumberOfObservations() > 0);
-//			System.out.println("There is at least one observation");
-
-			Assert.assertTrue("Same number of observations", firstDataSet.getNumberOfObservations() == secondDataSet.getNumberOfObservations());
-//			System.out.println("There is at least one observation");
-
-			int ddFieldIndex = firstDataSet.getFieldNames().indexOf("DD");
-			for (int i = 0; i < firstDataSet.getNumberOfObservations(); i++) {
-				double actualDD = (Double) secondDataSet.getValueAt(i, ddFieldIndex);
-				double expectedDD = (Double) firstDataSet.getValueAt(i, ddFieldIndex);
-				Assert.assertEquals("Testing degree-days above 5C",	expectedDD,	actualDD, 1E-8);
-			}
-		}
-		System.out.println("Degree-days above 5C successfully tested with multithreading!");
-		
+		System.out.println("Elapsed time = " + elapsedTime);
 	}
 
 
-	
+	public static void main(String[] args) throws BioSimClientException, BioSimServerException {
+		BioSimClientModelWithMultithreading.testingWithDegreeDaysAbove5C();
+	}
 	
 	
 }
