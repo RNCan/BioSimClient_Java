7196d43cfe5aab6b801663c24dc79c03d0288569
donenrique <50317186+donenrique24@users.noreply.github.com>
2020-10-02 13:55:24 +0000

    Cosmetic changes in the client + additional unitary tests

--- test/biosimclient/BioSimClientTestsWithReplicates.java
+++ test/biosimclient/BioSimClientTestsWithReplicates.java
@@ -21,6 +21,7 @@
  */
 package biosimclient;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -51,17 +52,63 @@ public class BioSimClientTestsWithReplicates {
 		testingFutureDegreeDaysWithRCP85andClimateModels(2015,2030,3);
 	}
 
-	
+
 	private static void testingFutureDegreeDaysWithRCP85andClimateModels(int initialDateYr, int finalDateYr, int nbReplicates) throws BioSimClientException, BioSimServerException {
 		List<BioSimPlot> locations = BioSimClientTestsOnNormals.getPlots();
 		
 		int expectedObservationsPerPlot = ((finalDateYr - initialDateYr) + 1) * nbReplicates;
-		LinkedHashMap<BioSimPlot, BioSimDataSet> oRCP85_RCM4def = BioSimClient.getModelOutput(initialDateYr, finalDateYr, locations, RCP.RCP85, null, "DegreeDay_Annual", nbReplicates, true, null);
+		LinkedHashMap<BioSimPlot, BioSimDataSet> oRCP85_RCM4def = BioSimClient.getModelOutput(initialDateYr, finalDateYr, locations, RCP.RCP85, null, "DegreeDay_Annual", nbReplicates, null);
 		
 		for (BioSimPlot plot : oRCP85_RCM4def.keySet()) {
 			BioSimDataSet firstDataSet = oRCP85_RCM4def.get(plot);
 			Assert.assertTrue("The number of observations", firstDataSet.getNumberOfObservations() ==  expectedObservationsPerPlot);
 		}
 	}
+
+	
+	@Test
+	public void test2015to2025_2repForcedClimateGeneration() throws BioSimClientException, BioSimServerException {
+		List<BioSimPlot> locations = new ArrayList<BioSimPlot>();
+		locations.add(BioSimClientTestsOnNormals.getPlots().get(0));
+		BioSimClient.setForceClimateGenerationEnabled(true);
+		LinkedHashMap<BioSimPlot, BioSimDataSet> oRCP85_RCM4def = BioSimClient.getModelOutput(2015, 2025, locations, RCP.RCP85, null, "DegreeDay_Annual", 2, null);
+		BioSimDataSet dataset = oRCP85_RCM4def.get(locations.get(0)); 
+		int repIndex = dataset.getFieldNames().indexOf("Rep");
+		int yearIndex = dataset.getFieldNames().indexOf("Year");
+		int refRep = -1;
+		int refYear = -1;
+		for (Observation obs : dataset.getObservations()) {
+			int rep = (Integer) obs.values.get(repIndex);
+			int year = (Integer) obs.values.get(yearIndex);
+			if (rep < refRep) {
+				Assert.fail("The ascending order was not repected in the Rep field");
+			} else  if (rep == refRep) {
+				if (year <= refYear) {
+					Assert.fail("The ascending order was not repected in the Year field");
+				} else {
+					refYear = year;
+				}
+			} else {
+				refRep = rep;
+				refYear = year;  
+			}
+		}
+		System.out.println("Ascending order tested in replicated generated climate!");
+		BioSimClient.resetClientConfiguration();
+	}
+
+
+	
+	@Test
+	public void test2012to2016_1repForcedClimateGeneration() throws BioSimClientException, BioSimServerException {
+		List<BioSimPlot> locations = new ArrayList<BioSimPlot>();
+		locations.add(BioSimClientTestsOnNormals.getPlots().get(0));
+		BioSimClient.setForceClimateGenerationEnabled(true);
+		LinkedHashMap<BioSimPlot, BioSimDataSet> oRCP85_RCM4def = BioSimClient.getModelOutput(2012, 2016, locations, RCP.RCP85, null, "DegreeDay_Annual", null);
+		BioSimDataSet dataset = oRCP85_RCM4def.get(locations.get(0)); 
+		Assert.assertEquals("Testing the number of observations", 5, dataset.getNumberOfObservations());
+		BioSimClient.resetClientConfiguration();
+	}
+
 	
 }
