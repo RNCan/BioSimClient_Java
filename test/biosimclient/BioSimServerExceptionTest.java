09686ad4f9e8406ff666b5e01a406043d4786c22
donenrique <50317186+donenrique24@users.noreply.github.com>
2020-10-19 20:11:03 +0000

    Better error handling. BioSimClient now handles the exception through the http codes.

--- test/biosimclient/BioSimServerExceptionTest.java
+++ test/biosimclient/BioSimServerExceptionTest.java
@@ -14,7 +14,7 @@ public class BioSimServerExceptionTest {
 
 	
 	@Test
-	public void incorrectRequestWithNaN() {
+	public void incorrectNormalsRequestWithNaN() {
 		BioSimFakeLocation fakeLocation = new BioSimFakeLocation(Double.NaN, Double.NaN, Double.NaN);
 		List<BioSimPlot> locations = new ArrayList<BioSimPlot>();
 		locations.add(fakeLocation);
@@ -32,7 +32,7 @@ public class BioSimServerExceptionTest {
 	}
 	
 	@Test
-	public void incorrectRequestWithInconsistentLatitudeAndLongitudeValues() {
+	public void incorrectNormalsRequestWithInconsistentLatitudeAndLongitudeValues() {
 		BioSimFakeLocation fakeLocation = new BioSimFakeLocation(-2000, 2000, Double.NaN);
 		List<BioSimPlot> locations = new ArrayList<BioSimPlot>();
 		locations.add(fakeLocation);
@@ -48,5 +48,54 @@ public class BioSimServerExceptionTest {
 			Assert.fail("Should have thrown a BioSimClientException instance");
 		}
 	}
+
 	
+	@Test
+	public void incorrectWeatherGenerationRequestWithInconsistentLatitudeAndLongitudeValues() {
+		BioSimFakeLocation fakeLocation = new BioSimFakeLocation(-2000, 2000, Double.NaN);
+		List<BioSimPlot> locations = new ArrayList<BioSimPlot>();
+		locations.add(fakeLocation);
+		try {
+			BioSimClient.getModelOutput(2000, 2001, locations, RCP.RCP45, ClimateModel.RCM4, "DegreeDay_Annual", null);
+			Assert.fail("Should have thrown a BioSimClientException instance");
+		} catch (BioSimClientException e) {
+			String errMsg = e.getMessage();
+			Assert.assertEquals("Testing exception", 
+					errMsg, 
+					"Code 400: Error: the latitude must range between -90.0 and 90.0, the longitude must range between -180.0 and 180.0");
+		} catch (Exception e) {
+			Assert.fail("Should have thrown a BioSimClientException instance");
+		}
+	}
+
+	@Test
+	public void incorrectModelHelpRequest() {
+		try {
+			BioSimClient.getModelHelp("Blabla");
+			Assert.fail("Should have thrown a BioSimClientException instance");
+		} catch (BioSimClientException e) {
+			String errMsg = e.getMessage();
+			Assert.assertEquals("Testing exception", 
+					errMsg, 
+					"Code 400: Error: Model Blabla does not exist");
+		} catch (Exception e) {
+			Assert.fail("Should have thrown a BioSimClientException instance");
+		}
+	}
+
+	@Test
+	public void incorrectModelDefaultParametersRequest() {
+		try {
+			BioSimClient.getModelDefaultParameters("Blabla");
+			Assert.fail("Should have thrown a BioSimClientException instance");
+		} catch (BioSimClientException e) {
+			String errMsg = e.getMessage();
+			Assert.assertEquals("Testing exception", 
+					errMsg, 
+					"Code 400: Error: Model Blabla does not exist");
+		} catch (Exception e) {
+			Assert.fail("Should have thrown a BioSimClientException instance");
+		}
+	}
+
 }
