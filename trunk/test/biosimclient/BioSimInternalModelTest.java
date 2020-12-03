3d00a0c409fc30fd67436b9494eb2ccb3c9ca7a4
donenrique <donenrique@8ab3efae-b829-4e4f-8a0c-45b54eb44eb3>
2020-12-03 19:31:52 +0000

    New reference files for internal model testing

--- trunk/test/biosimclient/BioSimInternalModelTest.java
+++ trunk/test/biosimclient/BioSimInternalModelTest.java
@@ -36,84 +36,89 @@ public class BioSimInternalModelTest {
 
 	
 	@Test
-	public void testingEachModel() throws NoSuchMethodException, SecurityException, BioSimClientException, BioSimServerException {
+	public void testingEachModelExceptPlanHardiness() throws NoSuchMethodException, SecurityException, BioSimClientException, BioSimServerException {
 		List<BioSimPlot> locations = new ArrayList<BioSimPlot>();
 		locations.add(new BioSimFakeLocation(45, -74, 300));
-		
+
+		List<String> blackList = new ArrayList<String>();
+		blackList.add("PlantHardinessCanada"); // because it needs 30 years of weather
+		blackList.add("PlantHardinessUSA"); // because it needs 30 years of weather
 		int nbFailures = 0;
 		int nbSuccesses = 0;
 		List<String> modelList = BioSimClient.getModelList();
 		for (String model : modelList) {
-			System.out.print("Testing model: " + model);
-			try {
-//				if (model.equals("MPB_SLR")) {
-//					int u = 0;
-//				}
-				Map<BioSimPlot, BioSimDataSet> output = BioSimClient.getModelOutput(2018, 
-						2019, 
-						locations, 
-						null, 
-						null, 
-						model, 
-						1, 
-						1, 
-						false, 
-						null);					
-				Assert.assertTrue("There is only one dataset in the output", output.size() == 1);
-				BioSimDataSet obsDataset = output.values().iterator().next();
-				List<Observation> observations = obsDataset.getObservations();
-				String filename = ObjectUtility.getPackagePath(getClass()).replace("bin", "test") + model + "ref.xml";
-//				UNCOMMENT THESE TWO LINES TO UPDATE THE TEST RESULTS
-//				XmlSerializer serializer = new XmlSerializer(filename);
-//				serializer.writeObject(observations);
-				
-				XmlDeserializer deserializer = new XmlDeserializer(filename);
-				List<Observation> references = (List) deserializer.readObject();
-				
-				Assert.assertEquals("Testing dataset have equal size", 
-						observations.size(), 
-						references.size());
-				
-				int nbSuccessful = 0;
-				int nbUnsuccessful = 0;
-				for (int i = 0; i < references.size(); i++) {
-					List<Object> refValues = references.get(i).values;
-					List<Object> obsValues = observations.get(i).values;
-					Assert.assertEquals("Testing observations have equal number of fields", 
-							refValues.size(), 
-							obsValues.size());
-					for (int j = 0; j < refValues.size(); j++) {
-						Object refValue = refValues.get(j);
-						Object obsValue = obsValues.get(j);
-						if (refValue instanceof Number && obsValue instanceof Number) {
-//							Assert.assertEquals("Testing values at line " + i + " field " + j, 
-//									((Number) refValue).doubleValue(), 
-//									((Number) obsValue).doubleValue(),
-//									1E-8);
-							if (Math.abs(((Number) refValue).doubleValue() - ((Number) obsValue).doubleValue()) < 1E-8) {
-								nbSuccessful++;
+			if (!blackList.contains(model)) {
+				System.out.print("Testing model: " + model);
+				try {
+//					if (model.equals("MPB_SLR")) {
+//						int u = 0;
+//					}
+					Map<BioSimPlot, BioSimDataSet> output = BioSimClient.getModelOutput(2018, 
+							2019, 
+							locations, 
+							null, 
+							null, 
+							model, 
+							1, 
+							1, 
+							false, 
+							null);					
+					Assert.assertTrue("There is only one dataset in the output", output.size() == 1);
+					BioSimDataSet obsDataset = output.values().iterator().next();
+					List<Observation> observations = obsDataset.getObservations();
+					String filename = ObjectUtility.getPackagePath(getClass()).replace("bin", "test") + model + "ref.xml";
+//					UNCOMMENT THESE TWO LINES TO UPDATE THE TEST RESULTS
+//					XmlSerializer serializer = new XmlSerializer(filename);
+//					serializer.writeObject(observations);
+					
+					XmlDeserializer deserializer = new XmlDeserializer(filename);
+					List<Observation> references = (List) deserializer.readObject();
+					
+					Assert.assertEquals("Testing dataset have equal size", 
+							observations.size(), 
+							references.size());
+					
+					int nbSuccessful = 0;
+					int nbUnsuccessful = 0;
+					for (int i = 0; i < references.size(); i++) {
+						List<Object> refValues = references.get(i).values;
+						List<Object> obsValues = observations.get(i).values;
+						Assert.assertEquals("Testing observations have equal number of fields", 
+								refValues.size(), 
+								obsValues.size());
+						for (int j = 0; j < refValues.size(); j++) {
+							Object refValue = refValues.get(j);
+							Object obsValue = obsValues.get(j);
+							if (refValue instanceof Number && obsValue instanceof Number) {
+//								Assert.assertEquals("Testing values at line " + i + " field " + j, 
+//										((Number) refValue).doubleValue(), 
+//										((Number) obsValue).doubleValue(),
+//										1E-8);
+								if (Math.abs(((Number) refValue).doubleValue() - ((Number) obsValue).doubleValue()) < 1E-8) {
+									nbSuccessful++;
+								} else {
+									nbUnsuccessful++;
+								}
 							} else {
-								nbUnsuccessful++;
-							}
-						} else {
-//							Assert.assertEquals("Testing observations have equal number of fields", 
-//									refValue, 
-//									obsValue);
-							if (refValue.equals(obsValue)) {
-								nbSuccessful++;
-							} else {
-								nbUnsuccessful++;
+//								Assert.assertEquals("Testing observations have equal number of fields", 
+//										refValue, 
+//										obsValue);
+								if (refValue.equals(obsValue)) {
+									nbSuccessful++;
+								} else {
+									nbUnsuccessful++;
+								}
 							}
 						}
 					}
+					nbSuccesses++;
+					int total = nbUnsuccessful + nbSuccessful;
+					System.out.println(" - Number of unsuccessful check = " + nbUnsuccessful + " / " + total);
+				} catch (Exception e) {
+					e.printStackTrace();
+					nbFailures++;
+					System.out.println(" - Failed for this reason " + e.toString());
 				}
-				nbSuccesses++;
-				int total = nbUnsuccessful + nbSuccessful;
-				System.out.println(" - Number of unsuccessful check = " + nbUnsuccessful + " / " + total);
-			} catch (Exception e) {
-				e.printStackTrace();
-				nbFailures++;
-				System.out.println(" - Failed for this reason " + e.toString());
 			}
 		}
 		System.out.println("Nb of failures = " + nbFailures + "; Nb of successes = " + nbSuccesses);
