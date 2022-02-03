d81d81654f44409741fe609d3d7c2568695c4e2b
donenrique <donenrique@8ab3efae-b829-4e4f-8a0c-45b54eb44eb3>
2022-02-03 15:21:34 +0000

    Enabling test with GrowingSeason model

--- trunk/src/biosimclient/BioSimClient.java
+++ trunk/src/biosimclient/BioSimClient.java
@@ -59,7 +59,7 @@ public final class BioSimClient {
 	static final String FieldSeparator = ",";
 	
 	private static final InetSocketAddress REpiceaAddress = new InetSocketAddress("repicea.dynu.net", 80);
-	private static final InetSocketAddress LocalAddress = new InetSocketAddress("localhost", 5000);	// BioSIM_B
+	private static final InetSocketAddress LocalAddress = new InetSocketAddress("192.168.0.194", 88);	// BioSIM_B
 	
 	private static final String SPACE_IN_REQUEST = "%20";
 
